e892d31c596ecad43789dd38468d55aabcee8b32
Risal Andika Tridisaputra <risal.andika@gmail.com>
2016-06-09 23:28:48 +0700

    no message

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $botToken = "236216584:AAHjRADsR3CmjpmtoduThokg5_onH-PXxs4";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($website."/getupdates");
+$update = file_get_contents("php://input");
 
 $updateArray = json_decode($update, TRUE);
 
