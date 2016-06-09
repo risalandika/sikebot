aae5532c6405074d12a338d1ff618c0c93c1d798
Risal Andika Tridisaputra <risal.andika@gmail.com>
2016-06-09 22:23:06 +0700

    asd

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 // Load composer
+
+
 require __DIR__ . '\vendor\autoload.php';
 
 $API_KEY = '212578930:AAHrcM7QUODJnl47uQeWgaKRiAItBmh2YV8';
 $BOT_NAME = 'si_kebot';
 $hook_url = 'https://sikebot.herokuapp.com/hook.php';
+echo $API_KEY;
+echo $BOT_NAME;
+echo $hook_url;
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
