5fc5c43d34fb0663ec13ac1b0b7612aa3963f004
Risal Andika Tridisaputra <risal.andika@gmail.com>
2016-06-09 23:19:10 +0700

    as

--- index.php
+++ index.php
@@ -7,5 +7,8 @@ $update = file_get_contents($website."/getupdates");
 
 $updateArray = json_decode($update, TRUE);
 
-print_r($updateArray);
+//$text = $updateArray["result"][0]["message"]["text"];
+$chatId = $updateArray["result"][0]["message"]["chat"]["id"];
+
+file_get_contents($website."/sendmessage?chat_id=".$chatId."$text=spotaker lu!");
 ?>
