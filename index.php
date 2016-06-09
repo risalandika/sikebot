ca6f73b8db94c42bf5e64054acf75bd9c3020964
Risal Andika Tridisaputra <risal.andika@gmail.com>
2016-06-09 23:39:59 +0700

    a

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 $botToken = "236216584:AAHjRADsR3CmjpmtoduThokg5_onH-PXxs4";
 $website = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents("php://input");
+$update = file_get_contents($website."/getupdates");
 
 $updateArray = json_decode($update, TRUE);
 
 //$text = $updateArray["result"][0]["message"]["text"];
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-//print_r($chatId);
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test!");
+print_r($chatId);
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=spotaker!");
 ?>
